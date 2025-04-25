1b452c08077fbbe0d1baee14bbf6b0bfe68410e1
Julieta <julieta.gobbi@fluxit.com.ar>
2025-04-25 15:19:16 -0300

    Change react version and type. Add ui-radix library and theme.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme accentColor="plum" grayColor="sand" radius="full">
+      <App />
+      <ThemePanel />
+    </Theme>
   </StrictMode>
 );
