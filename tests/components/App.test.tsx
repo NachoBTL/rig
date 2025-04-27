9c61a3d8c28004bd356050cd1e0c4016230f49b4
irojas <ignacio.rojas@fluxit.com.ar>
2025-04-27 19:52:44 -0300

    Adding husky config

---
+++ tests/components/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '@/App';
+
+describe('App', () => {
+  it('should render the component', () => {
+    render(<App />);
+
+    expect(screen.getByText('RIG')).toBeInTheDocument();
+  });
+});
