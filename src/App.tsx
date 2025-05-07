3b103357d7e42bf36966e20917d6d1c43a1551ad
Julieta <julieta.gobbi@fluxit.com.ar>
2025-05-07 12:55:35 -0300

    Creating popover component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text, Button } from '@radix-ui/themes';
+import Popover from '@/ui/atoms/Popover';
 
 export default function App() {
   return (
@@ -6,6 +7,22 @@ export default function App() {
       <Flex direction="column" gap="2">
         <Text>Hello from Radix Themes :)</Text>
         <Button>Let's go</Button>
+        <Flex direction="row" gap="2">
+          <Popover
+            trigger={<button>?</button>}
+            content={
+              <div>
+                <h4>Lorem ipsum</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur. Nunc volutpat elit dictumst lorem nullam
+                  ac quis gravida imperdiet. Neque aliquam vitae rhoncus aenean non aliquam aliquam
+                  morbi. In nibh mi tincidunt posuere volutpat tristique scelerisque.
+                </p>
+              </div>
+            }
+            side="right"
+          />
+        </Flex>
       </Flex>
     </>
   );
